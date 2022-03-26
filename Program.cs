6abd9133797b9b3acbf98b77ff5e5adb97a0e83c
torriery <andreastorrieri3@gmail.com>
2022-03-25 21:52:11 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿String nome,sobrenome;
+Console.Write("digite seu nome.....");
+nome=Console.ReadLine();
+Console.Write("digite seu sobrenome..");
+sobrenome=Console.ReadLine();
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
